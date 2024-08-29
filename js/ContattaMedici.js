3680275aaefd9f2ce9981377a36477a12cab0e6d
GabrielePusceddu <gabrielepusceddu21@gmail.com>
2024-08-29 12:54:35 +0200

    ContattaMedici

--- js/ContattaMedici.js
+++ js/ContattaMedici.js
@@ -276,6 +276,51 @@ fetch('http://localhost:8080/medici/searchBySpecializzazione?specializzazione=Oc
     });
 
 
+     // Funzione per ottenere i dati dal server
+     fetch('http://localhost:8080/medici/searchBySpecializzazione?specializzazione=Ortopedia')
+     .then(response => response.json())
+     .then(data => {
+         const tableBody = document.querySelector('#medici-table7 tbody');
+     
+         // Pulisce il corpo della tabella prima di aggiungere nuovi dati
+         tableBody.innerHTML = '';
+     
+         // Itera attraverso la lista dei medici e crea una riga per ciascuno
+         data.forEach(medico => {
+             const row = document.createElement('tr');
+     
+             // Crea le celle per ciascun campo del medico
+             const nomeCell = document.createElement('td');
+             nomeCell.textContent = medico.nome;
+     
+             const cognomeCell = document.createElement('td');
+             cognomeCell.textContent = medico.cognome;
+     
+             const specializzazioneCell = document.createElement('td');
+             specializzazioneCell.textContent = medico.specializzazione;
+     
+             const emailCell = document.createElement('td');
+             emailCell.textContent = medico.email;
+     
+             const telefonoCell = document.createElement('td');
+             telefonoCell.textContent = medico.telefono;
+     
+             // Aggiungi le celle alla riga
+             row.appendChild(nomeCell);
+             row.appendChild(cognomeCell);
+             row.appendChild(specializzazioneCell);
+             row.appendChild(emailCell);
+             row.appendChild(telefonoCell);
+     
+             // Aggiungi la riga al corpo della tabella
+             tableBody.appendChild(row);
+         });
+     })
+     .catch(error => {
+         console.error('Errore nel recuperare i dati dei medici:', error);
+     });
+
+
       // Funzione per ottenere i dati dal server
       fetch('http://localhost:8080/medici/searchBySpecializzazione?specializzazione=Odontoiatria')
       .then(response => response.json())
