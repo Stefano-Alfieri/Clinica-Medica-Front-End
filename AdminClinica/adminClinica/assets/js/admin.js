8d6e44c6f82d112f9b65ee9bb32adb251d81c236
Stefano Alfieri <stefanoalfieri7@gmail.com>
2024-08-30 11:24:01 +0200

    numeri schermata admin e nome amministratore

--- AdminClinica/adminClinica/assets/js/admin.js
+++ AdminClinica/adminClinica/assets/js/admin.js
@@ -1,3 +1,49 @@
+function init(){
+    isUserLoggedIn()
+    caricamentoNum()
+    getNomeUtente()
+}
+
+function caricamentoNum(){
+    const num1 = document.getElementById("num1");
+    const num2 = document.getElementById("num2");
+    const num3 = document.getElementById("num3");
+    
+    fetch('http://localhost:8080/medici/number')
+    .then(resp => resp.text())
+        .then(numero1 => {
+            num1.innerHTML = numero1;
+        });
+        
+    fetch('http://localhost:8080/pazienti/number')
+    .then(resp => resp.text())
+        .then(numero2 => {
+            num2.innerHTML = numero2;
+        });
+        
+        fetch('http://localhost:8080/personale/number')
+        .then(resp => resp.text())
+            .then(numero3 => {
+                num3.innerHTML = numero3;
+            });   
+             
+}
+function getNomeUtente(){
+    const nome= document.getElementById("nomeUtente");
+    const token = localStorage.getItem('authToken');
+    fetch('http://localhost:8080/auth/searchAdminByToken?token=' + token)
+        .then(resp => resp.text())
+        .then(adminId => {
+            fetch('http://localhost:8080/personale/' + adminId)   
+            .then(resp => resp.json())
+            .then(admin => {
+
+                nome.innerHTML =admin.nome+' '+admin.cognome;
+            });
+        });
+}
+
+
 async function isUserLoggedIn() {
     const token = localStorage.getItem('authToken');
     if (!token) {
@@ -16,4 +62,4 @@ async function isUserLoggedIn() {
 }
 
 
-window.onload = isUserLoggedIn;
+window.onload = init();
